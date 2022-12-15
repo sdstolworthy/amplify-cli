9c62952fff41d72c1e64513b5a83417425e0af72
Spencer Stolworthy <sdstolworthy@gmail.com>
2022-12-15 13:05:19 -0800

    ops: config type enum for commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-lerna-scopes', '@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [0, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'breaking', 'ops']],
+  },
 };
